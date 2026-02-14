1607ef0086d360255279262a65313b1c75e54382
Piyush Varman <varmanpiyush@gmail.com>
2026-02-14 15:16:12 +0530

    Updated responsive changes for mobile devices

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -45,7 +45,7 @@
       },[word,showCard]);
       return (
         <>
-          <div className='m-10 md:m-0 xl:m-0 flex flex-col place-items-center'>
+          <div className='m-5 md:m-0 xl:m-0 flex flex-col place-items-center'>
           <div className='flex flex-col place-items-center'>
             <h1 className='mt-[25vh] md:mt-[15vh] xl:mt-[22vh]  scale-200 text-5xl md:text-7xl xl:text-9xl font-bold text-transparent bg-linear-to-t from-black/90  to-white bg-clip-text'>LexiLog</h1>
             <p className='mt-[2vh] md:mt-[3vh] xl:mt-[5vh] text-4xl xl:text-5xl mb-10 font-["Inconsolata"]  text-gray-300 hover:scale-105 hover:text-white text-shadow-lg hover:cursor-none hover:text-shadow-white/10 transition-all duration-600 z-10'>A minimalistic <font className="font-['montserrat'] text-4xl xl:text-6xl bg-linear-to-t from-black-5 to-white bg-clip-text text-transparent"><strong>dictionary.</strong></font></p>
